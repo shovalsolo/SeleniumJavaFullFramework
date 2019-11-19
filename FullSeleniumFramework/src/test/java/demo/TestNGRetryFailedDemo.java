9857427202e6eef95d70b055344394fd7b4bdd91
shovalsolo <shovalso@gmail.com>
2019-11-19 17:19:22 -0500

    Adding TestNGRetryFailedDemo and RetryAnalyzer for automatically rerun failed test cases

---
+++ FullSeleniumFramework/src/test/java/demo/TestNGRetryFailedDemo.java
@@ -0,0 +1,42 @@
+/* 
+ * 1. Manually - run go to "test-output" folder find file "testing-failed.xml" and run manually again
+ * 2. Automated - 
+ * 3. After failure - 
+ * Will run the test cases and will fail in test2 and test3   
+ */
+
+package demo;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class TestNGRetryFailedDemo {
+	
+	@Test
+	public void test1() {
+		System.out.println("Hi from Test 1");
+	}
+	
+	@Test
+	public void test2() {
+		//System.out.println("Hi from Test 2 before the error");
+		int i=1/0;														//This test will fail
+		System.out.println("Hi from Test 2");
+		
+		
+	}
+	
+	@Test(retryAnalyzer = listeners.RetryAnalyzer.class)				//This will use "listeners.RetryAnalyzer.class" to rerun again the test
+	public void test3() {
+		//System.out.println("Hi from Test 3 before the error");
+		Assert.assertTrue(false);										//This test will fail
+		System.out.println("Hi from Test 3");
+	}
+	
+	@Test
+	public void test4() {
+		System.out.println("Hi from Test 4");
+	}
+
+}
---
+++ FullSeleniumFramework/src/test/java/listeners/RetryAnalyzer.java
@@ -0,0 +1,19 @@
+package listeners;
+
+import org.testng.IRetryAnalyzer;
+import org.testng.ITestResult;
+ 
+public class RetryAnalyzer implements IRetryAnalyzer {
+ 
+  private int retryCount = 0;								//Retries counter = 0
+  private static final int maxRetryCount = 3;				//Maximum retries = 3
+ 
+  
+  public boolean retry(ITestResult result) {				//Function that is returning true of false 
+    if (retryCount < maxRetryCount) {						//Checking if counter is less than maxRetryCount
+      retryCount++;											//If not less increasing the counter and running the retry again
+      return true;
+    }
+    return false;
+  }
+}
