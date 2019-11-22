47ad3344c1a81e6d37ae29f61e64c2eae3012c80
shovalsolo <shovalso@gmail.com>
2019-11-22 15:19:05 -0500

    Adding WindowSizeChromeDemo and control Arguments in the browser

---
+++ FullSeleniumFramework/src/test/java/demo/WindowSizeChromeDemo.java
@@ -0,0 +1,42 @@
+/* 
+ * Chrome window-size browser the option to control Arguments in the browser
+ */
+
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class WindowSizeChromeDemo {
+
+	public static WebDriver driver = null;
+
+	@Test
+	public static void test() {
+
+		WebDriverManager.chromedriver().setup();
+
+		
+		ChromeOptions options = new ChromeOptions();								//Creating an object of ChromeOptions						$$$
+		options.addArguments("window-size=1080,600");								//Setting the argument for window-size to options object	$$$
+
+	
+		driver = new ChromeDriver(options);											//Passing the options object the the ChromeDriver			$$$
+		
+		driver.get("https://google.com");											//Navigate to url
+		System.out.println(driver.getTitle());										//Will print the title of the page
+		driver.findElement(By.name("q")).sendKeys("Solotech");						//Sending keys to field
+		driver.findElement(By.name("q")).sendKeys(Keys.RETURN);						//Sending Enter key
+
+		driver.close();
+		driver.quit();
+		System.out.println("Test completed successfully");
+	}
+
+}
