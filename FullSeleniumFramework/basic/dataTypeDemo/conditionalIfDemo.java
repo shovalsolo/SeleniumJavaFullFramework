b24bfbd861557a713cb507e036211aeb4d69daa3
shovalsolo <shovalso@gmail.com>
2019-12-13 15:45:50 -0500

    Updating basic

---
+++ FullSeleniumFramework/basic/dataTypeDemo/conditionalIfDemo.java
@@ -0,0 +1,44 @@
+/* 
+ * This class is showing about if , else if and else conditional statement 
+ */
+
+package dataTypeDemo;
+
+public class conditionalIfDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 90;
+		String browser = "Chrome";
+		
+		System.out.println("Test started");
+		
+		if (i>90) {
+			System.out.println("Value is bigget then 90");
+		}
+		else if (i<90) {
+			System.out.println("Value is less then 90");
+		}
+		else {
+			System.out.println("Value is equal to 90");
+		}
+		
+		////////////////////////////////////////////////////
+		
+		
+		if (browser.equalsIgnoreCase("firefox")) {
+			System.out.println("Browser = firefox");
+		}
+		else if (browser.equalsIgnoreCase("ie")) {
+			System.out.println("Browser = Internet explorer");
+		}
+		else {
+			System.out.println("Browser = Chrome");
+		}
+		
+		
+		System.out.println("Test end");
+	}
+
+}
---
+++ FullSeleniumFramework/basic/dataTypeDemo/conditionalSwitchDemo.java
@@ -0,0 +1,37 @@
+/* 
+ * This class is showing about Switch case
+ */
+
+package dataTypeDemo;
+
+public class conditionalSwitchDemo {
+
+	public static void main(String[] args) {
+		
+		int x=4;
+		
+		switch (x) {
+		case 1:
+			System.out.println("Performence is poor x=1 ");
+			break;
+			
+		case 2:
+			System.out.println("Performence is slow x=2 ");
+			break;
+
+		case 3:
+			System.out.println("Performence is fair x=3 ");
+			break;
+			
+		case 4:
+			System.out.println("Performence is good x=4 ");
+			break;
+			
+		default:
+			System.out.println("out of scope ");
+			break;
+		}
+
+	}
+
+}
