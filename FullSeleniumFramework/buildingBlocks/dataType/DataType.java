7de0f864b9c3084f39e7d74ed80a971f4062ad9c
shovalsolo <shovalso@gmail.com>
2020-01-21 12:00:23 -0500

    Adding Building blocks package

---
+++ FullSeleniumFramework/buildingBlocks/dataType/DataType.java
@@ -0,0 +1,22 @@
+/* 
+ * This class is showing about java data types
+ */
+
+package dataType;
+
+public class DataType 
+{
+	public static void main(String[] args) 
+	{
+		// Non-object Data Types
+		int age=1;						//Does not allows decimal point (1)
+		
+		double price = 34.1;			//Does allows decimal point (1.5)
+		
+		boolean result= true;			//Only allows True or False
+		
+		
+		//Object Data Types
+		String customer = "John Doe";	//String needs to hold the content inside a ""
+	}
+}
