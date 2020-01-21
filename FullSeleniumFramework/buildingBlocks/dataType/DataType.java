83b3859e0ce3cfd24b2410b7e6ce35997ec4c481
shovalsolo <shovalso@gmail.com>
2020-01-21 16:29:59 -0500

    Updating DataType class

--- FullSeleniumFramework/buildingBlocks/dataType/DataType.java
+++ FullSeleniumFramework/buildingBlocks/dataType/DataType.java
@@ -1,8 +1,13 @@
 /* 
  * This class is showing about java data types
+ * Primitive - non object – normal values and values with numbers (integer, double, boolean, char, bit, short, long and float) – they all start with a lower case letters
+ * Class object – The data type is a class (String, WebDriver, WebElement) – They all start with upper case letters 
  */
 
 package dataType;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class DataType 
 {
@@ -14,9 +19,15 @@ public class DataType
 		double price = 34.1;			//Does allows decimal point (1.5)
 		
 		boolean result= true;			//Only allows True or False
-		
+
 		
 		//Object Data Types
 		String customer = "John Doe";	//String needs to hold the content inside a ""
+		
+		customer.charAt(0);				//We can add a dot only after a class and not on a primitive data type
+		
+		WebDriver driver;				//WebDriver as the class and the driver as the object - both of them will contain the same methods because the object is representing the class
+		
+		WebElement button;				//WebElement as the class and the button as the object - both of them will contain the same methods because the object is representing the class
 	}
 }
