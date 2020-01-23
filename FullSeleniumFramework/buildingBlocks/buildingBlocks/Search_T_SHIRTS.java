8c7880b742578fe7caff28da86c536479856ba3a
shovalsolo <shovalso@gmail.com>
2020-01-23 10:49:10 -0500

    Updating Search_T_SHIRTS

--- FullSeleniumFramework/buildingBlocks/buildingBlocks/Search_T_SHIRTS.java
+++ FullSeleniumFramework/buildingBlocks/buildingBlocks/Search_T_SHIRTS.java
@@ -28,31 +28,49 @@ public class Search_T_SHIRTS {
 			
 			driver = new ChromeDriver();												
 			
+			
+			//Open link
 			driver.get("http://automationpractice.com/index.php");						// Navigating to URL
 			
 			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);			//Setting timer to 10 seconds
 			
+			
+			//Click on Sign in
 			driver.findElement(By.linkText("Sign in")).click();							//Find by link text
 			Thread.sleep(5000);
 			
+			
+			//Send Keys to email field
 			driver.findElement(By.id("email")).sendKeys("Selenium@Automation.com");		//Find by id
 			Thread.sleep(5000);
 			
+			
+			//Send Keys to password field
 			driver.findElement(By.name("passwd")).sendKeys("SeleniumAutomation1234");	//Find by name
 			Thread.sleep(5000);
 			
+			
+			//Click on Login button
 			driver.findElement(By.cssSelector("#SubmitLogin")).click();					//Find by css Selector  Rclick and copy selector
 			Thread.sleep(5000);
 			
+			
+			//Click on Login SHIRTS
 			driver.findElement(By.partialLinkText("SHIRTS")).click();					//Can use to search only a part of the link text
 			Thread.sleep(5000);
 			
+			
+			//Send Keys to search field
 			driver.findElement(By.xpath("//*[@id=\"search_query_top\"]")).sendKeys("Orange");//Can use to search only a part of the link text
 			Thread.sleep(5000);
 			
+			
+			//Click on search 
 			driver.findElement(By.xpath("*//*[@type='submit']")).click();				//Can use to search only a part of the link text - Rclick and copy xpath selector
 			Thread.sleep(5000);
 			
+			
+			//Click on logout 
 			driver.findElement(By.className("logout")).click();							//Can use to search only a part of the link text
 			Thread.sleep(5000);
 			
