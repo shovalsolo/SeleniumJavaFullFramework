449344652aa3954b33a8f5110f6d6ab3292a9101
shovalsolo <shovalso@gmail.com>
2020-01-28 16:34:19 -0500

    Updating DriverInit comments

--- FullSeleniumFramework/src/test/java/singletoneWebDriver/DriverInit.java
+++ FullSeleniumFramework/src/test/java/singletoneWebDriver/DriverInit.java
@@ -7,7 +7,6 @@
 package singletoneWebDriver;
 
 import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.PageLoadStrategy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -20,7 +19,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverInit {
