62cd046f9e89dbbabc0d1aaffad7ec261adcf1e7
shovalsolo <shovalso@gmail.com>
2020-02-20 16:20:06 -0500

    Adding concat2Arrays

--- FullSeleniumFramework/basic/split2ArraysAndReturnUniquewords/SplitExample.java
+++ FullSeleniumFramework/basic/split2ArraysAndReturnUniquewords/SplitExample.java
@@ -14,7 +14,7 @@ public class SplitExample {
 		String str1 = new String("We are working today from home"); 		// This is string 1
 		String str2 = new String("Working from home is the best");			// This is string 2
 		
-		split2Strings(str1,str2);
+		split2Strings(str1,str2);											//Sending 2 strings to be converted to be 2 arrays
 	}
 	
 	public static void split2Strings(String str1, String str2) {
@@ -26,16 +26,34 @@ public class SplitExample {
 		
 		String array2[]= str2.split(" ");									//Splitting string2 to the array2
 		
+		System.out.println("The string after split");
 		printArray(array1);													//Printing the array
 		
 		printArray(array2);													//Printing the array
+		
+		concat2Arrays(array1,array2);
 	}
 	
 	public static void printArray(String[] str ) {
-		System.out.println("The string after split");
 		
 		for (int i = 0; i < str.length; i++) {
 			System.out.println(str[i]);
 		}
 	}
+	
+	public static void concat2Arrays(String[] str1,String[] str2) {
+		String[] result = new String[str1.length+str2.length];			//Setting the size to be dynamic
+		int i;
+		for (i = 0; i < str1.length; i++) {
+			result[i] = str1[i];
+		}
+		
+		int tempIndex = str1.length;
+		for (i = 0; i < str2.length; i++) {
+			result[tempIndex+i] = str2[i];
+		}
+		System.out.println("This is from concat 2 Arrays");
+		printArray(result);
+	}
+	
 }
