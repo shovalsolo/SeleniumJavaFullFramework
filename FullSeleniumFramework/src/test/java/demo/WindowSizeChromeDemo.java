e58d66dba3e9fd49a1efe906e95c784d8dccfb58
shovalsolo <shovalso@gmail.com>
2020-04-25 15:56:39 -0400

    Updating WindowSizeChromeDemo

--- FullSeleniumFramework/src/test/java/demo/WindowSizeChromeDemo.java
+++ FullSeleniumFramework/src/test/java/demo/WindowSizeChromeDemo.java
@@ -9,35 +9,77 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Reporter;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
+import screenShotPrintScreen.ScreenshotManager;
 
 public class WindowSizeChromeDemo {
 
 	public static WebDriver driver = null;
 
-	@Test
-	public static void test() {
-
-		WebDriverManager.chromedriver().setup();
-
-		
-		ChromeOptions options = new ChromeOptions();								//Creating an object of ChromeOptions						$$$
-		options.addArguments("window-size=1080,600");								//Setting the argument for window-size to options object	$$$
-
-	
-		driver = new ChromeDriver(options);											//Passing the options object the the ChromeDriver			$$$
-		
-		driver.get("https://google.com");											//Navigate to url
-		System.out.println(driver.getTitle());										//Will print the title of the page
-		driver.findElement(By.name("q")).sendKeys("benchsci");						//Sending keys to field
-		driver.findElement(By.name("q")).sendKeys(Keys.RETURN);						//Sending Enter key
-		
-
-		driver.close();
-		driver.quit();
-		System.out.println("Test completed successfully");
+	@BeforeClass																			//This will run before the class
+	public void setUp() {																	//Function that is calling the driver init
+		try {
+			Reporter.log("=======setUp Started=======",true);
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions options = new ChromeOptions();								//Creating an object of ChromeOptions						$$$
+			options.addArguments("window-size=1080,750");								//Setting the argument for window-size to options object	$$$
+			driver = new ChromeDriver(options);											//Passing the options object the the ChromeDriver			$$$								//Setting the browser selected and calling the function from the 
+			Reporter.log("=======setUp Ended=======",true);
+		} 
+		catch (Exception e) {
+			Reporter.log("=======setUp Failed=======",true);
+			ScreenshotManager.cuptureScreenshot(driver , "setUp-Failed");
+			e.printStackTrace();
+			System.out.println("Message is " + e.getMessage());								//Message of the exception
+			System.out.println("Cause is " + e.getCause());									//Cause of the exception
+		}
 	}
-
+	
+	@Test(priority = 1)
+	public static void login() {
+			
+		driver.get("http://automationpractice.com/index.php");											//Navigate to url
+		//System.out.println(driver.getTitle());												//Will print the title of the page
+		try {
+			driver.get("https://opensource-demo.orangehrmlive.com/");
+			Thread.sleep(2000);																//Wait to see the results
+			driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys("Admin");	//Sending keys to field
+			driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys("admin123");//Sending keys to field
+			driver.findElement(By.xpath("//input[@id='btnLogin']")).sendKeys(Keys.RETURN);	//Clicking on the button
+			Thread.sleep(5000);
+			Reporter.log("=======Login Ended=======",true);
+		} 
+		catch (InterruptedException e) {
+			Reporter.log("=======login Failed=======",true);
+			ScreenshotManager.cuptureScreenshot(driver , "login-Failed");
+			e.printStackTrace();
+			System.out.println("Message is " + e.getMessage());								//Message of the exception
+			System.out.println("Cause is " + e.getCause());
+		}																
+	}
+	
+	
+	@AfterClass																				//This will run after the class
+	public void tearDown() {																//Function to close the browser
+		try {
+			Reporter.log("=======tearDown Started=======",true);
+			driver.close();																	//Closing the browser
+			driver.quit();																	//Closing the process 
+			System.out.println("Test completed successfully");								//Printing message
+			Reporter.log("=======tearDown Ended=======",true);
+		} 
+		catch (Exception e) {
+			Reporter.log("=======tearDown Failed=======",true);
+			ScreenshotManager.cuptureScreenshot(driver , "tearDown-Failed");
+			e.printStackTrace();
+			System.out.println("Message is " + e.getMessage());								//Message of the exception
+			System.out.println("Cause is " + e.getCause());									//Cause of the exception
+		}
+	}
+	
+	
 }
