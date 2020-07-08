eae9a5da031bb60d3fc5e959ffd0c804a19142c0
shovalsolo <shovalso@gmail.com>
2020-07-08 15:42:12 -0400

    Adding let and const

---
+++ FullSeleniumFramework/ES6-JS/src/main.js
@@ -0,0 +1,54 @@
+// This is an example for let and const
+
+var a = 'Test1';
+let b = 'Test2';
+
+console.log(a);
+console.log(b);
+
+function testVar(){
+	var a = 30;
+	if (true){
+		var a = 50;
+		console.log(a);
+	}
+	console.log(a);
+}
+
+
+function testLet(){
+	let b = 30;				//the b var is valid only in this area and is equal to 50
+	console.log("b is equal in this area to:"+ b);
+	if (true){
+		let b = 50;			//the b var is valid only in this area and is equal to 50
+		console.log("b is equal in this area to:"+ b);
+	}
+	console.log("b is equal in this area to:"+ b);
+}
+
+testVar();
+testLet();
+
+
+for(var i = 0; i < 10 ; i ++){
+	console.log(i);
+}
+
+console.log("This is out side of the for loop: "+i);
+
+
+for(let j = 0; j < 10 ; j ++){
+	console.log(j);
+}
+
+//console.log("This is out side of the for loop: "+ j); // When using let inside the for loop j is not allowed out side 
+
+console.log("--------------------------------------------------");
+
+
+const colors = [];				//creating an colors array
+
+colors.push('red');				//inserting string in to the array
+colors.push('blue');
+
+console.log(colors);
