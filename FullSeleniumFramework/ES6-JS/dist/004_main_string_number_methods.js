4d3d5127b3df79562296f960858f1e51391d149e
shovalsolo <shovalso@gmail.com>
2020-07-13 14:58:26 -0400

    Updating string and number methods

--- FullSeleniumFramework/ES6-JS/dist/004_main_string_number_methods.js
+++ FullSeleniumFramework/ES6-JS/dist/004_main_string_number_methods.js
@@ -3,21 +3,26 @@
 // This is an example for string and number methods
 
 
+console.log('-------------Part 1---strings-----------');
+
 let theString = 'Hello, My name is John';
+let str = ' Graaaaaavy ';
 
 
 //startsWith();								//This method will return true/false when the string starts with a specific string
 //startsWith();								//This method will return true/false when the string ends with a specific string
 //includes();								//This method will return true/false when the string includes a specific string
+//repeat();									//This method will a string with the number you want
 
 
-console.log('the string starts with Hello?: '+theString.startsWith('Hello'));			//startsWith
-console.log('the string ends with John?: '+theString.endsWith('John'));					//endsWith
-console.log('the string includes My?: '+theString.includes('My'));						//includes
-console.log('the string includes JavaScript?: '+theString.includes('JavaScript'));		//includes
+console.log('the string starts with Hello?: '+ theString.startsWith('Hello'));			//startsWith
+console.log('the string ends with John?: '+ theString.endsWith('John'));				//endsWith
+console.log('the string includes My?: '+ theString.includes('My'));						//includes
+console.log('the string includes JavaScript?: '+ theString.includes('JavaScript'));		//includes
+console.log('the string will repeat: '+ str.repeat(5));									//repeat
 
 
-console.log('--------------------------------------');
+console.log('---------------Part 2-----numbers--------');
 
 
 console.log(0xFF);					//Equal 255 convert from Hexadecimal  
