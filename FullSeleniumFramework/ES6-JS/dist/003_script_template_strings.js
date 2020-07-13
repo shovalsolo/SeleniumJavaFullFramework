441efe7a789b21116be3007a7a25c5554bce452f
shovalsolo <shovalso@gmail.com>
2020-07-13 13:46:55 -0400

    Adding template strings

---
+++ FullSeleniumFramework/ES6-JS/dist/003_script_template_strings.js
@@ -0,0 +1,37 @@
+// This is an example for template strings we have to use `` backticks 
+// `` backticks does not ignore line brake 
+
+window.onload = function (){
+	
+	console.log("--------------part 1-----------");
+	
+	var temp = `Hello,
+	
+	 
+	 my name is ryu`;													//`` backticks does not ignore line brake and it will add it
+	
+	console.log(temp);
+	
+	
+	console.log("--------------part 2-----------");						//Same function without backticks
+	
+	
+	function logOld(name , age){
+		console.log("My name is: " + name + " and My age is: " + age);
+	}
+	
+	logOld("Dan", 64);
+	
+	
+	
+	console.log("--------------part 3-----------");						//Same function with backticks
+	
+	
+	function logNew(name , age){
+		console.log(`My name is: ${name} and My age is: ${age}`);
+	}
+	
+	logNew("Gil", 46);
+
+}
+
